b938ce92f953fbfbaefc79196cfab3e61bac8de5
Nigel Wade <Nigel.Wade7@gmail.com>
2012-03-13 11:17:48 -0400

    Add input and output directory

---
+++ path_join.py
@@ -0,0 +1,8 @@
+import os
+
+if os.path.exists(os.path.join(output_dir, filename)):
+	f = file(os.path.join(output_dir, filename))
+	path = os.path.join(output_dir,filename)
+if os.path.join(output_dir, filename)
+	f = file(path)
+
