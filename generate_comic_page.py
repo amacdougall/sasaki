b3042059dade0f021cc991522b6862a28ad68a2b
amacdougall <smoke@alanmacdougall.com>
2012-03-13 22:45:48 -0400

    Break site generation into functions to allow subtemplates.
    Main site generation is now contained in the generate_site() function,
    which can respond to certain tokens by calling content generation
    functions. These helper functions may run progressive token
    replacements, as with replace_tokens(), or call on a subtemplate to
    generate a sequence of lines, as with build_nav(). Notice how
    build_nav() creates a list of lines, and then collapses it to a string
    using "".join().
    
    Since the helper functions take the page data as an argument, they can
    change their own behavior based on the JSON data. build_nav(), for
    instance, loads a nav_template if one is specified, but defaults to
    "input_dir/nav_template.html" if not.

--- generate_comic_page.py
+++ generate_comic_page.py
@@ -1,25 +1,66 @@
 import os
 import json
 
-data_path = os.path.join("input_dir", "comic_data.json")
-data = json.load(open(data_path))
+def generate_site(data):
+    """
+    Generates the entire site from the supplied data. Data format is exemplified
+    by input_dir/comic_data.json.
+    """
 
-header = data["header"]
+    header = data["header"]
 
-for page in data["pages"]:
-    template_path = os.path.join("input_dir", "comic_template.html")
-    template = open(template_path)
+    for page in data["pages"]:
+        template_path = os.path.join("input_dir", "comic_template.html")
+        template = open(template_path)
 
-    output_path = os.path.join("output_dir", page["filename"])
-    output_file = open(output_path,"w")
+        output_path = os.path.join("output_dir", page["filename"])
+        output_file = open(output_path,"w")
 
-    for line in template:
-        line = line.replace("${header}", header)
-        line = line.replace("${page_title}", page["page_title"])
-        line = line.replace("${page_content}", page["page_content"])
+        for line in template:
+            if "${nav}" in line:
+                content = build_nav(page)
+            elif "${" in line:
+                content = replace_tokens(line, page, header)
+            else:
+                content = line
+
+            output_file.write(content)
+
+        template.close()
+        output_file.close()
+
+def replace_tokens(line, page, header):
+    """
+    Replaces standard tokens with page content, where found.
+    """
+    line = line.replace("${header}", header)
+    line = line.replace("${title}", page["title"])
+    line = line.replace("${content}", page["content"])
+    return line
+
+def build_nav(page):
+    """
+    Generates a navigation block for the supplied page.
+    """
+    if page.has_key("nav_template"):
+        nav_template_filename = page["nav_template"]
+    else:
+        nav_template_filename = "nav_template.html"
+
+    nav_template_path = os.path.join("input_dir", nav_template_filename)
+    nav_template = open(nav_template_path)
+
+    content_lines = []
+
+    for line in nav_template:
         line = line.replace("${next_page}", page["next_page"])
         line = line.replace("${previous_page}", page["previous_page"])
-        output_file.write(line)
+        content_lines.append(line)
 
-    template.close()
-    output_file.close()
+    nav_template.close()
+    return "".join(content_lines)
+
+# generate the site
+data_path = os.path.join("input_dir", "comic_data.json")
+data = json.load(open(data_path))
+generate_site(data)
