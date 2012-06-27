befe341085a792462246700bd83043dd09b480fe
amacdougall <smoke@alanmacdougall.com>
2012-06-26 22:33:32 -0400

    Remove raw_comic_images from version control.

--- path_join.py
+++
@@ -1,8 +0,0 @@
-import os
-
-if os.path.exists(os.path.join(output_dir, filename)):
-	f = file(os.path.join(output_dir, filename))
-	path = os.path.join(output_dir,filename)
-if os.path.join(output_dir, filename)
-	f = file(path)
-
